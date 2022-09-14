68e611137e9cd65e93345b5ac652b74ec49a28a8
Milan Toth <milgra@milgra.com>
2022-09-14 11:14:50 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# brawl
-Mass Brawl!!!
+# Brawl - Mass Brawl!!!
+
+![Brawl](brawl.png)
+
+### TODO
+
+- re-write physics and collosion based on cljs-brawl
---
+++ src/brawl/pivot.c
@@ -0,0 +1,45 @@
+
+#ifndef parabola_types_h
+#define parabola_types_h
+
+#include "math2.c"
+
+typedef struct _pivot_t pivot_t;
+struct _pivot_t
+{
+
+    char* id;
+    v2_t  position;
+};
+
+pivot_t* pivot_alloc(char* id, v2_t position);
+
+void pivot_destruct(void* pointer);
+
+#endif /* parabola_types_h */
+
+#if __INCLUDE_LEVEL__ == 0
+
+#include "mtcstr.c"
+#include "mtmem.c"
+#include "mtstr.c"
+
+void pivot_destruct(void* pointer)
+{
+
+    pivot_t* pivot = pointer;
+    mtmem_release(pivot->id);
+}
+
+pivot_t* pivot_alloc(char* id, v2_t position)
+{
+
+    pivot_t* pivot = mtmem_alloc(sizeof(pivot_t), pivot_destruct);
+
+    pivot->id       = mtcstr_fromcstring(id);
+    pivot->position = position;
+
+    return pivot;
+}
+
+#endif
